2dada5b71fc28e61b01a38299c27f99b7a44b3a3
MattVoda <mattvodakov@gmail.com>
2017-07-19 21:59:04 -0400

    first commit

---
+++ Assets/Scripts/Grow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour {
+
+    
+    public float sizeMultiplier = 1.1f;
+    public float maxX = 2f;
+    public float maxY = 2f;
+    public float maxZ = 2f;
+    public float timeInSeconds = 1.0f;
+    public Vector3 scaleVector = new Vector3(2f,2f,2f);
+    
+    public bool growNow = false;
+    private bool hasGrown = false;
+
+    private Transform cubeMeshTransform;
+    
+    private void Awake() {
+        cubeMeshTransform = transform.parent.Find("CubeMesh");
+        scaleVector = cubeMeshTransform.localScale;
+    }
+    
+    void Start () { }
+	
+	
+	void Update () {
+
+        if (growNow && !hasGrown) {
+            hasGrown = true;
+            iTween.ScaleTo(cubeMeshTransform.gameObject, iTween.Hash("scale", scaleVector, "time", timeInSeconds, "easeType", iTween.EaseType.easeOutSine));
+        } else if (!growNow && hasGrown) {
+            hasGrown = false;
+            iTween.ScaleTo(cubeMeshTransform.gameObject, iTween.Hash("scale", Vector3.one, "time", timeInSeconds, "easeType", iTween.EaseType.easeOutSine));
+        }
+
+	}
+
+
+    //void ScaleValues() {
+    //    if(scaleVector.x < maxX) {
+    //        scaleVector.x = scaleVector.x * sizeMultiplier;
+    //    }
+    //    if (scaleVector.y < maxY) {
+    //        scaleVector.y = scaleVector.y * sizeMultiplier;
+    //    }
+    //    if (scaleVector.z < maxZ) {
+    //        scaleVector.z = scaleVector.z * sizeMultiplier;
+    //    }
+    //}
+}
---
+++ Assets/Scripts/InteractableItem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class InteractableItem : MonoBehaviour {
+
+    private bool isScaled = false;
+
+    public Rigidbody rigidbody;
+    private bool currentlyInteracting;
+
+    public float velocityFactor = 1f;
+    private Vector3 posDelta;
+
+    private Quaternion rotationDelta;
+    private float rotationFactor = 4f;
+    private float angle;
+    private Vector3 axis;
+
+    private WandController attachedWand;
+    private Grow growScript;
+    private Transform interactionPoint;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        interactionPoint = new GameObject().transform; //an empty object's transform
+        velocityFactor = rigidbody.mass;  //realistic -- bigger mass = smaller velocity factor
+
+        growScript = gameObject.GetComponent<Grow>();
+	}
+	
+	void Update () {
+	    //make it do stuff!
+        if (attachedWand && currentlyInteracting) {
+            growScript.growNow = true;
+        } else {
+            growScript.growNow = false;
+        }
+	}
+
+    public void BeginInteraction(WandController wand) {
+        attachedWand = wand;
+        interactionPoint.position = wand.transform.position;
+        interactionPoint.rotation = wand.transform.rotation;
+        interactionPoint.SetParent(transform, true);
+
+        currentlyInteracting = true;
+    }
+
+    public void EndInteraction(WandController wand) {
+        if (wand == attachedWand) {
+            attachedWand = null;
+            currentlyInteracting = false;
+        }
+    }
+
+    public bool IsInteracting() {
+        return currentlyInteracting;
+    }
+}
+
+
+//posDelta = attachedWand.transform.position - interactionPoint.position;
+//this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+//rotationDelta = attachedWand.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+//rotationDelta.ToAngleAxis(out angle, out axis);
+
+//if (angle > 180) { //if you have a big rotation to make, take the path of least resistance
+//    angle -= 360;
+//}
+
+//this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
