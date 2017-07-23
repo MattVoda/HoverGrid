bcf02b34f219f2b7cfe5e63162ca3d8e5d60c616
MattVoda <mattvodakov@gmail.com>
2017-07-23 16:44:16 -0400

    VRTK fully hooked up! Rising cubes.

---
+++ Assets/Scripts/Riser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Riser : MonoBehaviour {
+
+
+    public float riseDistanceMultiplier = 3f;
+    public float time = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartRise() {
+        StartCoroutine(Rise(true));
+    }
+    public void EndRise() {
+        StartCoroutine(Rise(false));
+    }
+
+    IEnumerator Rise(bool riseNow) {
+
+        if (riseNow) {
+            for (float i = 0; i <= time; i += Time.deltaTime) {
+                gameObject.transform.localPosition = new Vector3(0, i * riseDistanceMultiplier, 0);
+                yield return null;
+            }
+        }
+
+        // fall
+        else {
+            for (float i = time; i >= 0; i -= Time.deltaTime) {
+                gameObject.transform.localPosition = new Vector3(0, i * riseDistanceMultiplier, 0);
+                yield return null;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/tk_listener_Grow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class tk_listener_Grow : MonoBehaviour {
+
+    private void Start() {
+        if (GetComponent<VRTK_InteractTouch>() == null) {
+            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
+            return;
+        }
+
+        GetComponent<VRTK_InteractTouch>().ControllerStartTouchInteractableObject += new ObjectInteractEventHandler(DoTouchStart);
+        GetComponent<VRTK_InteractTouch>().ControllerTouchInteractableObject += new ObjectInteractEventHandler(DoTouch);
+        GetComponent<VRTK_InteractTouch>().ControllerStartUntouchInteractableObject += new ObjectInteractEventHandler(DoUntouchStart);
+        GetComponent<VRTK_InteractTouch>().ControllerUntouchInteractableObject += new ObjectInteractEventHandler(DoUntouch);
+        
+    }
+
+    private void DoTouchStart(object sender, ObjectInteractEventArgs e) {
+        e.target.transform.parent.GetComponentInChildren<Riser>().StartRise();
+        //e.target.GetComponent<Riser>().StartRise();
+    }
+
+    private void DoTouch(object sender, ObjectInteractEventArgs e) {
+
+    }
+
+    private void DoUntouchStart(object sender, ObjectInteractEventArgs e) {
+
+    }
+
+    private void DoUntouch(object sender, ObjectInteractEventArgs e) {
+        e.target.transform.parent.GetComponentInChildren<Riser>().EndRise();
+        //e.target.GetComponent<Riser>().StartRise();
+    }
+
+}
